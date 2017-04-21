7a6d312f76d8c817346718675d8ee8e4fb8725ac
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-04-21 19:22:12 +0300

    Server setup

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-import 'express';
+import express from 'express';
+import http from 'http';
+import bodyParcer from 'body-parser';
+import morgan from 'morgan';
 
+const app = express();
+
+// App Setup
+
+
+// Server Setup
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on port '+port);
