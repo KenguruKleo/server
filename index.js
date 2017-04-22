835fec12ac8fa40ebcaa561c58ad7dc966139858
kostya.yemelianov <kostya.yemelianov@ab-soft.net>
2017-04-22 10:40:09 +0300

    add router

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import http from 'http';
 import bodyParcer from 'body-parser';
 import morgan from 'morgan';
+import router from './router';
 
 const app = express();
 
 // App Setup
+app.use( morgan('combined') );
+app.use( bodyParcer.json({type: '*/*'}) );
 
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 8080;
